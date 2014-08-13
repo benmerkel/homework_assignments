74e5d5d27be1f0444a51e106d4a9d16a46db23d4
Ben Merkel <ben@merkelfamily.com>
2014-08-12 22:49:07 -0600

    Updated to handle non-pluralizing when 1 bottle

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -2,16 +2,17 @@
 
 starting_bottles = 100
 
-def recursion (bottles)
-  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer."
+def recursion (bottles, plural)
+  puts "#{bottles} bottle#{plural} of beer on the wall, #{bottles} bottle#{plural} of beer."
   bottles -= 1
-  puts "Take one down and pass it around, #{bottles} bottles of beer on the wall.\n\n"
+  (bottles == 1) ? plural = '' : plural = 's'
+  puts "Take one down and pass it around, #{bottles} bottle#{plural} of beer on the wall.\n\n"
   if bottles > 0
-    recursion(bottles)
+    recursion(bottles, plural)
   else
-    puts "No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall."
+    puts 'No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.'
   end
 end
 
-recursion(starting_bottles)
+recursion(starting_bottles, 's')
