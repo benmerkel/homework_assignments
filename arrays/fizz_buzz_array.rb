4854af0f0b5eb50cd1b1b1d14ccdbe345e1987cc
Ben Merkel <ben@merkelfamily.com>
2014-08-12 22:47:42 -0600

    Updated fizz_buzz_array.rb code to use array methods.

--- arrays/fizz_buzz_array.rb
+++ arrays/fizz_buzz_array.rb
@@ -1,15 +1,21 @@
 #!/usr/bin/env ruby
 
 array = (1..100).to_a
-
-array.each {|value|
-  if (value % 3).zero? && (value % 5).zero?
-  puts 'FizzBuzz'
-  elsif (value % 3).zero?
-    puts 'Fizz'
-  elsif (value % 5).zero?
-    puts 'Buzz'
-  else
-    puts value
-  end
+fizz_buzz = array.map { |value|
+  ((value % 3).zero? && (value % 5).zero?) ? 'FizzBuzz' :
+    ((value % 3).zero?) ? 'Fizz' :
+      ((value % 5).zero?) ? 'Buzz' : value
 }
+
+puts fizz_buzz
+
+# if (value % 3).zero? && (value % 5).zero?
+#   puts 'FizzBuzz'
+#   elsif (value % 3).zero?
+#     puts 'Fizz'
+#   elsif (value % 5).zero?
+#     puts 'Buzz'
+#   else
+#     puts value
+#   end
+# }
